9daa77eb5d5b5bdc45fb8a6915633e4946040c5a
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-02-18 11:29:08 +0600

    Add print statement

---
+++ AVFoundationBasic/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AVFoundationBasic
+//
+//  Created by Mac PC on 2/18/20.
+//  Copyright © 2020 Ticon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+    }
+
+
+}
+
