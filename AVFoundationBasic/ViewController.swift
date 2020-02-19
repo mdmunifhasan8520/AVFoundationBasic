acc44dbdf0af23e006b498b54ddda7307a10fb19
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-02-19 11:46:30 +0600

    add play and pause button and a timer.

--- AVFoundationBasic/ViewController.swift
+++ AVFoundationBasic/ViewController.swift
@@ -13,16 +13,46 @@ class ViewController: UIViewController {
     
     //Create audio Player Object
     var audioPlayer = AVAudioPlayer()
+    var isPlaying = false
+    var timer: Timer!
 
     @IBOutlet weak var playedTime: UILabel!
-    
     @IBOutlet weak var PlaySoundLabel: UIButton!
     
+    // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         playAudio()
        
     }
+    
+    @IBAction func stopMusic(_ sender: Any) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0
+        isPlaying = false
+        
+    }
+    
+    @IBAction func playOrPauseMusic(_ sender: Any) {
+        if isPlaying {
+            audioPlayer.stop()
+            isPlaying = false
+        } else {
+            audioPlayer.play()
+            isPlaying = true
+            
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func updateTime() {
+        let currentTime = Int(audioPlayer.currentTime)
+        let minutes = currentTime/60
+        let seconds = currentTime - minutes * 60
+        
+        playedTime.text = String(format: "%02d:%02d", minutes,seconds) as String
+    }
+    
     func playAudio() {
         //sound File
         let soundURL = Bundle.main.path(forResource: "C#", ofType: ".wav")
@@ -33,12 +63,6 @@ class ViewController: UIViewController {
         } catch {
             print("error")
         }
-    }
-
-    @IBAction func PlaySound(_ sender: Any) {
-        audioPlayer.play()
-        PlaySoundLabel.titleLabel?.text = "Stop Sound"
-        print("play sound")
     }
     
 }
