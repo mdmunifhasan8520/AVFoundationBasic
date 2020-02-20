8f2e1f1ea1810835ef5b65c537f185fd948139d1
mdmunifhasan8520 <mdmunifhasan8520@gmail.com>
2020-02-20 11:53:19 +0600

    implement a basic slider for the playback track.

--- AVFoundationBasic/ViewController.swift
+++ AVFoundationBasic/ViewController.swift
@@ -11,26 +11,31 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    //Create audio Player Object
+    //Create a audio Player Object
     var audioPlayer = AVAudioPlayer()
-    var isPlaying = false
-    var timer: Timer!
-
+    
+    var isPlaying = false //This bool determines if the music is currently playing
+    var timer: Timer! // timer property is needed for dispalying the current time of the music track
+    
+    
     @IBOutlet weak var playedTime: UILabel!
     @IBOutlet weak var PlaySoundLabel: UIButton!
+    @IBOutlet weak var playBackSlider: UISlider!
+    
     
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         playAudio()
-       
+        playBackSlider.maximumValue = Float(audioPlayer.duration)
+        
+        var timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(upadteSlider), userInfo: nil, repeats: true)
     }
     
     @IBAction func stopMusic(_ sender: Any) {
         audioPlayer.stop()
         audioPlayer.currentTime = 0
         isPlaying = false
-        
     }
     
     @IBAction func playOrPauseMusic(_ sender: Any) {
@@ -44,6 +49,12 @@ class ViewController: UIViewController {
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
         }
     }
+    @IBAction func changeAudioTime(_ sender: Any) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = TimeInterval(playBackSlider.value)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
     
     @objc func updateTime() {
         let currentTime = Int(audioPlayer.currentTime)
@@ -53,9 +64,14 @@ class ViewController: UIViewController {
         playedTime.text = String(format: "%02d:%02d", minutes,seconds) as String
     }
     
+    @objc func upadteSlider() {
+        playBackSlider.value = Float(audioPlayer.currentTime)
+    }
+    
     func playAudio() {
         //sound File
-        let soundURL = Bundle.main.path(forResource: "C#", ofType: ".wav")
+        //let soundURL = Bundle.main.path(forResource: "C#", ofType: ".wav")
+        let soundURL = Bundle.main.path(forResource: "atmosphere", ofType: ".mp3")
         
         do {
             try audioPlayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: soundURL!))
